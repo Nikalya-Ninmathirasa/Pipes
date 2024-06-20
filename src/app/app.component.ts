10f2c790bbb38cd712258b6f15eb419f21297458
Nikalya_1398 <v.ninmathirasa@gmail.com>
2024-06-20 20:51:21 +0530

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Pipes';
+  value=50;
+  decimal =2.55555;
+  jsondata={name:"niru",age:30}
+  date=Date();
+
+  convert(value:number,rate:number){
+    return value*rate;
+  }
 }
